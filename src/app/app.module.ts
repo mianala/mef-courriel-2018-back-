b220ee582095efa5eba56e16eea9131d7dd0bd25
Loharano <mianalaloharano@gmail.com>
2018-06-28 10:15:33 +0300

    pagination of -> sur

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -101,18 +101,18 @@ export class MatPaginatorIntlCro extends MatPaginatorIntl {
   nextPageLabel = 'Page suivante';
   previousPageLabel = 'Page précédente';
 
-  /*  getRangeLabel = function (page, pageSize, length) {
-      if (length === 0 || pageSize === 0) {
-        return '0 od ' + length;
-      }
-      length = Math.max(length, 0);
-      const startIndex = page * pageSize;
-      // If the start index exceeds the list length, do not try and fix the end index to the end.
-      const endIndex = startIndex < length ?
-        Math.min(startIndex + pageSize, length) :
-        startIndex + pageSize;
-      return startIndex + 1 + ' - ' + endIndex + ' od ' + length;
-    };*/
+  getRangeLabel = function (page, pageSize, length) {
+    if (length === 0 || pageSize === 0) {
+      return '0 sur ' + length;
+    }
+    length = Math.max(length, 0);
+    const startIndex = page * pageSize;
+    // If the start index exceeds the list length, do not try and fix the end index to the end.
+    const endIndex = startIndex < length ?
+      Math.min(startIndex + pageSize, length) :
+      startIndex + pageSize;
+    return startIndex + 1 + ' - ' + endIndex + ' sur ' + length;
+  };
 
 }
 
