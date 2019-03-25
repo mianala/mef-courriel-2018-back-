e5852df5a8d98cee277b498279e90894c81eb57e
Loharano <mianalaloharano@gmail.com>
2019-03-25 11:54:19 +0300

    logo and title

---
+++ src/app/service/env.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class EnvService {
+
+  static ip() {
+    return 'http://localhost:5000'
+    // return 'http://192.168.90.90:5000'
+    // return 'http://192.2.27.69:3000'
+  }
+
+  constructor() { }
+
+}
